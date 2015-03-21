f831291420ab5e3cd2a41aeed087e6fb9c09b13b
Graham Robinson <graham@yelcho.com.au>
2015-03-21 17:26:23 +1100

    Change height

--- Component.js
+++ Component.js
@@ -5,5 +5,5 @@ jQuery.sap.declare("samples.components.pinky.Component");
 sap.ui.core.UIComponent.extend("samples.components.pinky.Component", {});
 
 samples.components.pinky.Component.prototype.createContent = function() {
-    return new sap.m.Image({src: "img/pinky.jpg"});
+    return new sap.m.Image({src: "img/pinky.jpg", height:"400px"});
 };
