1ede2ad0cc55165f8e9e0f93284798ac8b81d255
Graham Robinson <graham@yelcho.com.au>
2015-03-21 16:39:18 +1100

    Initial commit

---
+++ Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.require("sap.ui.core.UIComponent");
+jQuery.sap.require("sap.m.Image");
+jQuery.sap.declare("samples.components.pinky.Component");
+
+sap.ui.core.UIComponent.extend("samples.components.pinky.Component", {});
+
+samples.components.pinky.Component.prototype.createContent = function() {
+    return new sap.m.Image({src: "img/pinky.jpg"});
+};
